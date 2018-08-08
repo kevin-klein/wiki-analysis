dd428d9a9dbb4602eea29574b909ae815ebd4985
Kevin Klein <kevin@tabolo.de>
2018-08-08 21:27:08 +0200

    work from today

--- graph.py
+++ graph.py
@@ -2,6 +2,8 @@ import sqlite3
 import csv
 import networkx as nx
 import re
+import pandas as pd
+import numpy as np
 # import matplotlib.pyplot as plt
 from networkx.readwrite import json_graph
 import json
@@ -17,20 +19,21 @@ def dict_factory(cursor, row):
 class Page(object):
 
     def __init__(self, d):
+        # print(d)
         self.title = d['title']
         self.namespace = d['namespace']
         self.raw_content = d['raw_content']
-        self.created_at = d['created_at']
-        self.updated_at = d['updated_at']
+        # self.created_at = d['created_at']
+        # self.updated_at = d['updated_at']
 
     def full_title(self):
         return '{}:{}'.format(self.namespace, self.title)
 
     def links(self):
-        return re.findall(r'\[\[([^]]+)\]\]', self.raw_content)
+        return re.findall(r'\[\[([^]]+)\]\]', str(self.raw_content))
 
     def __repr__(self):
-        return self.full_title() + " " + self.updated_at
+        return self.full_title() #+ " " + self.updated_at
 
 class PageChange(Page):
 
@@ -66,12 +69,12 @@ def get_changes():
     conn.close()
     return changes
 
-def process_wiki_time(item):
-    t, scope = item
-    page_list = []
-
-    for _, page in scope.items():
-        page_list.append(page)
+def create_graph(page_list):
+    # t, scope = item
+    # page_list = []
+    #
+    # for _, page in scope.items():
+    #     page_list.append(page)
 
     G = nx.Graph()
     for page in page_list:
@@ -92,9 +95,11 @@ def process_wiki_time(item):
                 if G.has_node(link):
                     G.add_edge(page.full_title(), link, name='linksTo')
 
-    d = json_graph.node_link_data(G)
-    with open('graphs/graph_' + t.replace(':', '').replace('.', '').replace(' ', '') + '.json', 'w') as f:
-        json.dump(d, f)
+    # d = json_graph.node_link_data(G)
+    # with open('graphs/graph_' + t.replace(':', '').replace('.', '').replace(' ', '') + '.json', 'w') as f:
+    #     json.dump(d, f)
+
+    return G
 
     # pos = nx.spring_layout(G, k=0.15,iterations=20)
     # nx.draw(G, pos=pos, with_labels=False)
@@ -139,17 +144,54 @@ def read_json_file(filename):
     return json_graph.node_link_graph(js_graph)
 
 if __name__ == '__main__':
-    G = read_json_file('graphs/graph_2017-08-22183008786189.json')
+    # G = read_json_file('graphs/graph_2017-08-22183008786189.json')
+    #
+
+    data = pd.read_csv('pages.csv')
+    pages = list(data.apply(lambda x: Page(x.to_dict()), axis=1))
+
+    # print(pages)
+    G = create_graph(pages)
 
     bc = networkx.algorithms.betweenness_centrality(G)
 
-    with open('betweenness_centrality.json', 'w') as f:
-        json.dump(bc, f, indent=4)
+    # with open('betweenness_centrality.json', 'w') as f:
+    #     json.dump(bc, f, indent=4)
 
     G.remove_edges_from(nx.selfloop_edges(G))
+    kcores = networkx.algorithms.core_number(G)
 
-    with open('core_numbers.json', 'w') as f:
-        json.dump(networkx.algorithms.core_number(G), f, indent=4)
+    lines = {}
+
+    for page in pages:
+        lines[page.full_title()] = len(str(page.raw_content).split('\n'))
+
+
+    metrics = pd.DataFrame({'bc': bc,'kcores': kcores,'lines': lines})#.transpose()
+    # print(metrics)
+    # metrics = metrics.rename({0: 'betweenness_centrality', 1: 'k-core'}, axis='columns')
+
+    tags = pd.read_csv('tags.csv')
+    tags['idx'] = tags.apply(lambda x: '{}:{}'.format(x['namespace'], x['title']),axis=1)
+    tags = tags.set_index('idx')
+
+    data = pd.concat([tags,metrics], axis=1, join='inner')
+    print(data.corr())
+
+
+
+
+
+    # with open('core_numbers.json', 'w') as f:
+    #     json.dump(networkx.algorithms.core_number(G), f, indent=4)
+
+    # data = pd.read_csv('pages.csv')
+    # data['link'] = data.apply(lambda x: 'http://101wiki.softlang.org/{}:{}'.format(x['namespace'], x['title']).replace(' ', '_'), axis=1)
+    # data['tag'] = np.NaN
+    #
+    # sample = data.sample(50)
+    #
+    # sample.to_csv('sample.csv', index=False)
 
     # changes = get_changes()
     # pages = get_pages()
