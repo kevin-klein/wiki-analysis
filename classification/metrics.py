96e695d6ae0a1f6705c561e6510c3f03dd3e586c
johanneshaertel <johanneshaertel@uni-koblenz.de>
2018-10-29 18:02:54 +0100

    metrics

--- classification/metrics.py
+++ classification/metrics.py
@@ -10,11 +10,17 @@ import networkx.algorithms.centrality
 import networkx.algorithms.community as cmt
 from collections import defaultdict
 
+pd.options.display.max_rows = 50
+pd.options.display.max_columns = 15
+pd.options.display.width = 500
+
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
+
 
 class Page(object):
 
@@ -33,7 +39,8 @@ class Page(object):
         return re.findall(r'\[\[([^]]+)\]\]', str(self.raw_content))
 
     def __repr__(self):
-        return self.full_title() #+ " " + self.updated_at
+        return self.full_title()  # + " " + self.updated_at
+
 
 class PageChange(Page):
 
@@ -41,7 +48,9 @@ class PageChange(Page):
         Page.__init__(self, d)
         self.page_id = d['page_id']
 
+
 from multiprocessing import Pool
+
 
 def get_pages():
     conn = sqlite3.connect('db.sqlite3')
@@ -49,12 +58,14 @@ def get_pages():
     cursor = conn.cursor()
 
     pages = []
-    for page in cursor.execute('select * from pages where pages.id not in(select page_id from page_changes)').fetchall():
+    for page in cursor.execute(
+            'select * from pages where pages.id not in(select page_id from page_changes)').fetchall():
         pages.append(Page(page))
 
     conn.close()
 
     return pages
+
 
 def get_changes():
     conn = sqlite3.connect('db.sqlite3')
@@ -63,11 +74,13 @@ def get_changes():
 
     changes = []
 
-    for change in cursor.execute('select * from page_changes where page_changes.raw_content <> \'\' order by created_at').fetchall():
+    for change in cursor.execute(
+            'select * from page_changes where page_changes.raw_content <> \'\' order by created_at').fetchall():
         changes.append(PageChange(change))
 
     conn.close()
     return changes
+
 
 def create_graph(page_list):
     # t, scope = item
@@ -138,10 +151,12 @@ def create_graph(page_list):
     #     if len(documents) == 5:
     #         raise
 
+
 def read_json_file(filename):
     with open(filename) as f:
         js_graph = json.load(f)
     return json_graph.node_link_graph(js_graph)
+
 
 def split_by_namespace(data):
     result = defaultdict(dict)
@@ -154,6 +169,7 @@ def split_by_namespace(data):
 
     return result
 
+
 def calculate_averages(data):
     result = {}
     for namespace, ns_data in data.items():
@@ -162,45 +178,54 @@ def calculate_averages(data):
 
     return result
 
+
 if __name__ == '__main__':
     # G = read_json_file('graphs/graph_2017-08-22183008786189.json')
     #
 
-    data = pd.read_csv('similarity/pages.csv')
+    data = pd.read_csv('../similarity/pages.csv')
     pages = list(data.apply(lambda x: Page(x.to_dict()), axis=1))
 
     G = create_graph(pages)
-    G.remove_edges_from(nx.selfloop_edges(G))
+    # G.remove_edges_from(nx.selfloop_edges(G))
+    G.remove_edges_from(G.selfloop_edges())
 
     algorithms = [
         networkx.algorithms.betweenness_centrality,
         networkx.algorithms.load_centrality,
         # networkx.algorithms.dispersion,
         networkx.algorithms.degree_centrality,
-        networkx.algorithms.core_number]
+        networkx.algorithms.core_number
+    ]
 
-    result = {}
+    metrics = pd.DataFrame({a.__name__: a(G) for a in algorithms})
 
-    for a in algorithms:
-        print(a.__name__)
-        data = a(G)
+    metrics['namespace'] = metrics.index.map(lambda x: x.split(':')[0].strip())
+    metrics['title'] = metrics.index.map(lambda x: x.split(':')[1].strip())
 
-        bc = split_by_namespace(data)
+    print(metrics)
 
-        result[a.__name__] = {}
-        result[a.__name__]['data'] = bc
-        result[a.__name__]['average'] = calculate_averages(bc)
+    print(metrics.groupby('namespace').mean().rank(axis=0, ascending=False))
 
-    with open('data/data.json', 'w') as f:
-        json.dump(result, f, indent=4)
+    #     print(a.__name__)
+    #
+    #
+    #     bc = split_by_namespace(data)
+    #
+    #     result[a.__name__] = {}
+    #     result[a.__name__]['data'] = bc
+    #     result[a.__name__]['average'] = calculate_averages(bc)
+    #
+    # with open('data/data.json', 'w') as f:
+    #     json.dump(result, f, indent=4)
 
-        # with open('betweenness_centrality_average.json', 'w') as f:
-        #     json.dump(calculate_averages(bc), f, indent=4)
-        #
-        # kcores = networkx.algorithms.core_number(G)
-        #
-        # with open('kcores.json', 'w') as f:
-        #     json.dump(kcores, f, indent=4)
+    # with open('betweenness_centrality_average.json', 'w') as f:
+    #     json.dump(calculate_averages(bc), f, indent=4)
+    #
+    # kcores = networkx.algorithms.core_number(G)
+    #
+    # with open('kcores.json', 'w') as f:
+    #     json.dump(kcores, f, indent=4)
 
     # lines = {}
     #
